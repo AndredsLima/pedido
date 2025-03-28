19a327a2cf8d53d185aed420e622451c74a5889e
André de Sousa Lima <137451523+AndredsLima@users.noreply.github.com>
2025-03-27 21:04:56 -0300

    Fix execption PrecessarPedido

--- src/main/java/pedido/controller/PedidoController.java
+++ src/main/java/pedido/controller/PedidoController.java
@@ -1,6 +1,8 @@
 package pedido.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,13 @@ public class PedidoController {
     private PedidoService pedidoService;
 
     @PostMapping
-    public String criarPedido(@RequestBody PedidoDTO pedidoDTO) {
-//
-        pedidoService.processarPedido(pedidoDTO);
-        return "Pedido criado e enviado para o sistema de estoque!";
+    public ResponseEntity<String> criarPedido(@RequestBody PedidoDTO pedidoDTO) {
+        boolean enviado = pedidoService.processarPedido(pedidoDTO);
+
+        if (enviado) {
+            return ResponseEntity.ok("Pedido criado e enviado para o sistema de estoque!");
+        } else {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body("Serviço de estoque temporariamente indisponível");
+        }
     }}
--- src/main/java/pedido/service/PedidoService.java
+++ src/main/java/pedido/service/PedidoService.java
@@ -1,7 +1,7 @@
 package pedido.service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,24 +11,22 @@ import pedido.dto.PedidoDTO;
 @Service
 public class PedidoService {
 
+    private static final Logger log = LoggerFactory.getLogger(PedidoService.class);
+
     @Autowired
     private AmazonSQS amazonSQS;
 
     @Value("${aws.sqs.queue.url}")
     private String queueUrl;
 
-    public void processarPedido(PedidoDTO pedidoDTO) {
+    public boolean processarPedido(PedidoDTO pedidoDTO) {
         try {
-
-
             String mensagem = new ObjectMapper().writeValueAsString(pedidoDTO);
-            System.out.println("Mensagem serializada: " + mensagem);
-
-
-            amazonSQS.sendMessage(new SendMessageRequest(queueUrl, mensagem));
-            System.out.println("Mensagem enviada para a fila SQS");
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            amazonSQS.sendMessage(queueUrl, mensagem);
+            return true;
+        } catch (Exception e) {
+            log.error("Erro ao enviar para SQS", e);
+            return false;
         }
     }
 }
